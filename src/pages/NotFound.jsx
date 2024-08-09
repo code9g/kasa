e6ce5365792cdffcefa6fb277a3236d5947b1bdf
Pierre HENRY <pierre.henry@code99.fr>
2024-08-09 02:16:53 +0200

    Page NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
-  return <main>404</main>;
+  return (
+    <main className="not-found">
+      <div className="container">
+        <h2 className="title">404</h2>
+        <p className="message">Oups! La page que vous demandez n'existe pas.</p>
+        <Link className="link" to="/home">
+          Retourner sur la page d'accueil
+        </Link>
+      </div>
+    </main>
+  );
 }
 
 export default NotFound;
