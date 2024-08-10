6bd2391fe6c92e5f0fafe4528a72882d51d98047
Pierre HENRY <pierre.henry@code99.fr>
2024-08-10 07:12:46 +0200

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,21 @@ import NotFound from "./pages/NotFound";
 import Rental from "./pages/Rental";
 import "./scss/main.scss";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        { path: "", element: <Navigate to="/home" /> },
-        { path: "home", element: <Home /> },
-        { path: "about", element: <About /> },
-        { path: "rental/:id", element: <Rental /> },
-        { path: "*", element: <NotFound /> },
-      ],
-    },
-  ],
-  { basename: "/kasa" }
-);
+const routes = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Navigate to="/home" /> },
+      { path: "home", element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "rental/:id", element: <Rental /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes, { basename: "/kasa" });
 
 function App() {
   return <RouterProvider router={router} />;
